b561e506123c5e1a4abbb1039304d92d7f806241
anindyariza <11251017@student.itk.ac.id>
2026-05-11 10:52:11 +0800

    first commit

---
+++ src/main/java/com/mycompany/sistempantai/Sistempantai.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistempantai;
+
+/**
+ *
+ * @author anin
+ */
+public class Sistempantai {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+        
